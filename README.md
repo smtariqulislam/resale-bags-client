070b747ac2893eb15043090b39cb54c53d866427
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-11-24 15:55:45 +0600

    router setup and header section working

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Project setup :
 
+## install:
 1. npx create-react-app 
 2. git init
 3. tailwind
 4. react-router-dom
+5. react icon  
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Router/Router';
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+   <RouterProvider router={router}></RouterProvider>
   );
 }
 
