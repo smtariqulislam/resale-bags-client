a4b16daa1aab3374bf5ee07197f861e950490727
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-11-29 19:47:39 +0600

    spinner add

--- src/Router/PrivateRoute.js
+++ src/Router/PrivateRoute.js
@@ -1,17 +1,24 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 import { AuthContext } from '../contexts/AuthProvider';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(loading);
+  if (loading) {
+    return (
+      <div className="h-screen">
+        <Spinner />
+      </div>
+    );
+  }
 
-    const {user} =useContext(AuthContext);
-    const location =useLocation();
-
-    if(user){
-        return children;
-    }
-
-    return <Navigate to='/login' state={{from:location}} replace></Navigate>
+  if (user && user.uid) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
